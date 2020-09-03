1f7e40bd15ef28ffc6ef5a0350c8bea611d06422
David Hatch <david@osohq.com>
2020-09-03 16:51:34 -0400

    Use templates.

--- oso_social/social/views.py
+++ oso_social/social/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 from .models import Post
 
@@ -9,9 +8,4 @@ def list_posts(request):
     # Limit to 10 latest posts.
     posts = Post.objects.all()[:10]
 
-    posts_text = ""
-
-    for post in posts:
-        posts_text += f"@{post.created_by} {post.contents}"
-
-    return HttpResponse(posts_text)
+    return render(request, 'social/list.html', {'posts': posts})
