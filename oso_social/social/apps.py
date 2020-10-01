633f494c00dbc3c7cf8a3e32afac6a69d86afb8b
David Hatch <david@osohq.com>
2020-10-01 10:50:55 -0400

    Setup date.

--- oso_social/social/apps.py
+++ oso_social/social/apps.py
@@ -1,5 +1,18 @@
 from django.apps import AppConfig
 
+from datetime import datetime, timedelta
+
+from django_oso.oso import Oso
+
+class Date:
+    @classmethod
+    def ten_min_ago(cls):
+        return datetime.now() - timedelta(minutes=10)
+
 
 class SocialConfig(AppConfig):
     name = 'social'
+
+    def ready(self):
+        print("ready")
+        Oso.register_class(Date)
